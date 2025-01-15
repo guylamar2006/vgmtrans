3bc6fe8969ee3e0c676d616d10161f852501593d
guylamar2006 <guychronister@outlook.com>
2025-01-15 13:38:57 -0600

    Add missing return value for bool.

--- src/main/formats/CPS/CPS1Instr.cpp
+++ src/main/formats/CPS/CPS1Instr.cpp
@@ -230,5 +230,9 @@ std::string CPS1OPMInstrSet::generateOPMFile() {
 
 bool CPS1OPMInstrSet::saveAsOPMFile(const std::string &filepath) {
   auto content = generateOPMFile();
-  pRoot->UI_writeBufferToFile(filepath, reinterpret_cast<uint8_t*>(const_cast<char*>(content.data())), static_cast<uint32_t>(content.size()));
+  if (content.empty()) {
+    return false;
+  }
+  bool success = pRoot->UI_writeBufferToFile(filepath, reinterpret_cast<uint8_t*>(const_cast<char*>(content.data())), static_cast<uint32_t>(content.size()));
+  return success;
 }
